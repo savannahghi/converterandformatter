15aceb0585df84cc11e7f5dff67951a249053020
maxwellgithinji <maxwellgithinji@gmail.com>
2021-07-21 19:56:03 +0300

    chore: move converter function

--- converters.go
+++ converters.go
@@ -4,10 +4,13 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math"
 	"math/big"
 	"strconv"
 	"time"
+
+	"github.com/savannahghi/serverutils"
 )
 
 // StructToMap converts an object (struct) to a map.
@@ -40,4 +43,29 @@ func GenerateRandomWithNDigits(numberOfDigits int) (string, error) {
 // one main be.well@bewell.co.ke email account
 func GenerateRandomEmail() string {
 	return fmt.Sprintf("be.well+%v@bewell.co.ke", time.Now().Unix())
+}
+
+// ConvertInterfaceMap converts a map[string]interface{} to a map[string]string.
+//
+// Any conversion errors are written out to the output map instead of being
+// returned as error values.
+//
+// New code is discouraged from using this function.
+func ConvertInterfaceMap(inp map[string]interface{}) map[string]string {
+	out := make(map[string]string)
+	if inp == nil {
+		return out
+	}
+	for k, v := range inp {
+		val, ok := v.(string)
+		if !ok {
+			val = fmt.Sprintf("invalid string value: %#v", v)
+			if serverutils.IsDebug() {
+				log.Printf(
+					"non string value in map[string]interface{} that is to be converted into map[string]string: %#v", v)
+			}
+		}
+		out[k] = val
+	}
+	return out
 }
